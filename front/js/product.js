29272ac18205a91612a3ed72f78d0e589532f96c
David952 <david.amorim@live.fr>
2022-06-14 15:35:22 +0200

    Mise à jour du script "product"

--- front/js/product.js
+++ front/js/product.js
@@ -5,21 +5,19 @@
 */
 const url = new URLSearchParams(window.location.search);
 const id = url.get("id");
-console.log(id);
 /*
 *
-* Récupération de tous les produits de l'API
+* Récupération de l'ID des produits de l'API
 *
 */
-fetch("http://localhost:3000/api/products/")
+fetch(`http://localhost:3000/api/products/${id}`)
     // Retour du résultat en JSON.
     .then((result) => result.json())
-    // On nomme le résulat apiItems.
-    .then((apiItems) => {
+    // On nomme le résulat apiProduct.
+    .then((apiProduct) => {
         // On affiche ce qui a été récupéré sous forme tableau dans la console.
-        console.table(apiItems);
-        //Fonction affichage du produit
-        displayProduct(apiItems);
+        // Fonction affichage du produit
+        displayProduct(apiProduct);
     })
     // Une erreur est survenue si détecté
     .catch((error) => {
@@ -37,20 +35,16 @@ function displayProduct(product) {
     const price = document.getElementById('price');
     const description = document.getElementById('description');
     const colors = document.getElementById('colors');
-    // Une boucle pour rechercher chaque élément
-    for (let display of product) {
-        //On vérifie que l'id du produit correspond bien au produit choisit dans l'API
-        if (id === display._id) {
-            // On définit les éléments qui va être dynamique
-            image.innerHTML += `<img src="${display.imageUrl}" alt="${display.altTxt}">`;
-            title.textContent = `${display.name}`;
-            price.textContent = `${display.price}`;
-            description.textContent = `${display.description}`;
-            //On met une boucle pour parcourir les couleurs disponible pour chaque produit associé
-            for (let color of display.colors) {
-                //On ajoute les options couleurs avec leurs valeurs
-                colors.innerHTML += `<option value="${color}">${color}</option>`;
-            }
-        }
+    
+    // On modifie nos éléments en appliquant les valeurs stockées dans l'API
+    image.innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+    title.textContent = `${product.name}`;
+    price.textContent = `${product.price}`;
+    description.textContent = `${product.description}`;
+
+    // On met une boucle pour parcourir les couleurs disponible pour chaque produit associé
+    for (let color of product.colors) {
+        // On ajoute les options couleurs avec leurs valeurs
+        colors.innerHTML += `<option value="${color}">${color}</option>`;
     }
 }
