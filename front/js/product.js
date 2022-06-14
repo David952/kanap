708ab3f4655e318b76a18ac2e8c929855d6e95b9
David952 <david.amorim@live.fr>
2022-06-14 13:11:08 +0200

    Mise à jour du script "product"

--- front/js/product.js
+++ front/js/product.js
@@ -11,23 +11,46 @@ console.log(id);
 * Récupération de tous les produits de l'API
 *
 */
-fetch("http://localhost:3000/api/products")
+fetch("http://localhost:3000/api/products/")
     // Retour du résultat en JSON.
-    .then((resultat) => resultat.json())
-    // On nomme le résulat apiProduits.
-    .then((apiProduits) => {
+    .then((result) => result.json())
+    // On nomme le résulat apiItems.
+    .then((apiItems) => {
         // On affiche ce qui a été récupéré sous forme tableau dans la console.
-        console.table(apiProduits);
+        console.table(apiItems);
+        //Fonction affichage du produit
+        displayProduct(apiItems);
     })
     // Une erreur est survenue si détecté
     .catch((error) => {
-        alert(error)
+        console.log(error);
     });
-    /*
+/*
 *
 * Fonction d'affichage du produit sur la page
 *
 */
-function Produits(produit) {
-
+function displayProduct(product) {
+    // Création des varibles de ciblage des éléments
+    const image = document.getElementById('item__img');
+    const title = document.getElementById('title');
+    const price = document.getElementById('price');
+    const description = document.getElementById('description');
+    const colors = document.getElementById('colors');
+    // Une boucle pour rechercher chaque élément
+    for (let display of product) {
+        //On vérifie que l'id du produit correspond bien au produit choisit dans l'API
+        if (id === display._id) {
+            // On définit les éléments qui va être dynamique
+            image.innerHTML += `<img src="${display.imageUrl}" alt="${display.altTxt}">`;
+            title.textContent = `${display.name}`;
+            price.textContent = `${display.price}`;
+            description.textContent = `${display.description}`;
+            //On met une boucle pour parcourir les couleurs disponible pour chaque produit associé
+            for (let color of display.colors) {
+                //On ajoute les options couleurs avec leurs valeurs
+                colors.innerHTML += `<option value="${color}">${color}</option>`;
+            }
+        }
+    }
 }
