cb19daa022c047ed5f5dad14d595f5260ce3a374
David952 <david.amorim@live.fr>
2022-06-14 13:10:35 +0200

    Mise à jour du script "index"

--- front/js/script.js
+++ front/js/script.js
@@ -5,36 +5,41 @@
 */
 fetch("http://localhost:3000/api/products")
     // Retour du résultat en JSON.
-    .then((resultat) => resultat.json())
-    // On nomme le résulat apiProduits.
-    .then((apiProduits) => {
+    .then((result) => result.json())
+    // On nomme le résulat apiItems.
+    .then((apiItems) => {
         // On affiche ce qui a été récupéré sous forme tableau dans la console.
-        console.table(apiProduits);
+        console.table(apiItems);
         // Fonction d'affichage des produits de l'API
-        articlesProduits(apiProduits);
+        displayItems(apiItems);
     })
     // Une erreur est survenue si détecté
     .catch((error) => {
-        alert(error)
+        console.log(error);
     });
 /*
 *
 * Fonction d'affichage des produits sur la page
 *
 */
-function articlesProduits(index) {
+function displayItems(items) {
+    // On pointe une variable en mémoire
+    const documentFragment = document.createElement('div');
     // Création de la variable de la section des articles
-    let articles = document.getElementById('items');
+    let itemsContainer = document.getElementById('items');
     // Boucle pour 'article' dans index
-    for (let article of index) {
+    for (let item of items) {
         // Création et ajout des articles
-        articles.innerHTML += `
-        <a href="./product.html?id=${article._id}">
-            <article>
-                <img src="${article.imageUrl}" alt="${article.altTxt}">
-                <h3 class="productName">${article.name}1</h3>
-                <p class="productDescription">${article.description}</p>
-            </article>
-        </a>`
+        documentFragment.innerHTML += `
+            <a href="./product.html?id=${item._id}">
+                <article>
+                    <img src="${item.imageUrl}" alt="${item.altTxt}">
+                    <h3 class="productName">${item.name}</h3>
+                    <p class="productDescription">${item.description}</p>
+                </article>
+            </a>
+        `
     }
+    // 
+    itemsContainer.innerHTML = documentFragment.innerHTML;
 }
