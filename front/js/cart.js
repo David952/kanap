11b9e87107e9d7bea8f03d3c38d9afbdc93346cf
David952 <david.amorim@live.fr>
2022-06-21 14:05:18 +0200

    Mise à jour du script 'cart'

--- front/js/cart.js
+++ front/js/cart.js
@@ -17,3 +17,45 @@ fetch("http://localhost:3000/api/products/")
     .catch((error) => {
         console.log(error);
     });
+
+/*
+*
+* 
+*
+*/
+
+function displayCart(cart) {
+// On crée un élément <div> en mémoire
+const documentFragment = document.createElement('div');
+// On récupére l'élément items situé dans le DOM
+let itemsContainer = document.getElementById('cart__items');
+
+// 
+for (let cart of carts) {
+
+    // 
+    documentFragment.innerHTML += `
+        <article class="cart__item" data-id="${cart._id}" data-color="${cart.colors}">
+            <div class="cart__item__img">
+            <img src="${cart._image}" alt="${cart._alt}">
+            </div>
+            <div class="cart__item__content">
+            <div class="cart__item__content__description">
+                <h2>${cart.name}</h2>
+                <p>${cart.colors}</p>
+                <p>${cart.price}</p>
+            </div>
+            <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                <p>Qté : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cart.quantity}">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
+                </div>
+            </div>
+            </div>
+        </article>
+    `
+    }
+}
