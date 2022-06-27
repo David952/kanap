718d6f15b9d63b8b6e96fad712dd93be37ab2cad
David952 <david.amorim@live.fr>
2022-06-27 12:59:01 +0200

    Mise à jour de 'cart.js'

--- front/js/cart.js
+++ front/js/cart.js
@@ -17,17 +17,16 @@ fetch("http://localhost:3000/api/products/")
     });
 
 /**
-*
+* Fonction d'affichage du panier
 */
 function displayCart(carts) {
-// On crée un élément <div> en mémoire
-const documentFragment = document.createElement('div');
+// On crée un élément <article> en mémoire
+const documentFragment = document.createElement('article');
 // On récupére l'élément items situé dans le DOM
 let itemsContainer = document.getElementById('cart__items');
 
-// 
+// On boucle les éléments de notre panier
 for (let cart of carts) {
-    // 
     documentFragment.innerHTML += `
         <article class="cart__item" data-id="${cart._id}" data-color="${cart.colors}">
             <div class="cart__item__img">
@@ -52,4 +51,7 @@ for (let cart of carts) {
         </article>
     `
     }
+    // On insère la string concaténée directement dans le contenu de l'élément du DOM
+    // L'opération d'insertion ou de modification ne s'effectue qu'une seule fois
+    itemsContainer.innerHTML = documentFragment.innerHTML;
 }
