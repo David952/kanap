ec2db57eec356907805c1f2b94172496a25b6698
David952 <david.amorim@live.fr>
2022-07-12 16:39:29 +0200

    Mise à jour du script "product.js"

--- front/js/product.js
+++ front/js/product.js
@@ -1,51 +1,74 @@
-/**
- * Importation des fonctions du script "ls.js"
- */
-import {localStorageHas, localStorageSave, localStorageGet} from './ls.js';
+import { localStorageHas, localStorageSave, localStorageGet } from './ls.js';
 
-/*
-*
+/**
 * On récupère l'id du produit grâce à l'URL
-*
+* On définit les variables qui serviront dans notre script
 */
 const url = new URLSearchParams(window.location.search);
 const id = url.get("id");
 const PRODUCT_KEY_LOCALSTORAGE = 'products';
+const title = document.getElementById('title');
+const colors = document.getElementById('colors');
+const description = document.getElementById('description');
+const quantity = document.getElementById('quantity');
+const price = document.getElementById('price');
 
-/*
-*
-* Récupération de l'ID des produits de l'API
-*
-*/
-fetch(`http://localhost:3000/api/products/${id}`)
-    // Retour du résultat en JSON.
-    .then((result) => result.json())
-    // On nomme le résulat apiProduct.
-    .then((apiProduct) => {
-        // On affiche ce qui a été récupéré sous forme tableau dans la console.
-        // Fonction affichage du produit
-        displayProduct(apiProduct);
-    })
-    // Une erreur est survenue si détecté
-    .catch((error) => {
-        console.log(error);
+
+/**
+ *
+ * @returns {Promise<void>}
+ */
+async function init() {
+    // Récupération de l'ID des produits de l'API
+    await fetch(`http://localhost:3000/api/products/${id}`)
+        // Retour du résultat en JSON.
+        .then((result) => result.json())
+        // On nomme le résulat apiProduct.
+        .then((apiProduct) => {
+            // On affiche ce qui a été récupéré sous forme tableau dans la console.
+            // Fonction affichage du produit
+            displayProduct(apiProduct);
+        })
+        // Une erreur est survenue si détecté
+        .catch((error) => {
+            console.log(error);
+        });
+
+    // Ajout du produit au localStorage (panier). On définit les variables à prendre en compte pour le localStorage.
+    const addProductCart = document.getElementById('addToCart');
+    const image = document.getElementById('image');
+        // Les variables sont au début du fichier
+
+    // La fonction va nous permettre d'ajouter un produit dans le localStorage
+    addProductCart.addEventListener('click', () => {
+        const product = {
+            id: id,
+            quantity: quantity.value,
+            colors: colors.value,
+            price: Number(price.innerHTML),
+            title: title.innerHTML,
+            image: image.src,
+            imageAlt: image.alt,
+        }
+
+        addProductToLocalStorage(product);
     });
 
-/*
-*
-* Fonction d'affichage du produit sur la page
-*
-*/
+    // Changer le status du bouton lorsque le select change de valeur
+    colors.addEventListener('change', btnChange);
+}
+
+/**
+ * Fonction d'affichage du produit sur la page
+ * @param {Object} product
+ */
 function displayProduct(product) {
     // Création des varibles de ciblage des éléments
-    const imageAlt = document.getElementById('item__img');
-    const title = document.getElementById('title');
-    const price = document.getElementById('price');
-    const description = document.getElementById('description');
-    const colors = document.getElementById('colors');
-    
+    const imageContainer = document.getElementById('item__img');
+        // Les variables sont au début du fichier
+
     // On modifie nos éléments en appliquant les valeurs stockées dans l'API
-    imageAlt.innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+    imageContainer.innerHTML += `<img id="image" src="${product.imageUrl}" alt="${product.altTxt}" />`;
     title.textContent = `${product.name}`;
     price.textContent = `${product.price}`;
     description.textContent = `${product.description}`;
@@ -57,39 +80,9 @@ function displayProduct(product) {
     }
 }
 
-/*
-*
-* Ajout du produit au localStorage (panier)
-*
-*/
-
-// On définit les variables à prendre en compte pour le localStorage.
-const addProductCart = document.getElementById('addToCart');
-const quantity = document.getElementById('quantity');
-const colors = document.getElementById('colors');
-const price = document.getElementById('price');
-const title = document.getElementById('title');
-const imageAlt = document.getElementById('imageAlt');
-
-// La fonction va nous permettre d'ajouter un produit dans le localStorage
-addProductCart.onclick = () =>{
-    const product = {
-        id: id,
-        quantity: quantity.value,
-        colors: colors.value,
-        price: price.value,
-        title: title.value,
-        /*imageAlt: imageAlt.value,*/
-    }
-
-    addProductToLocalStorage(product);
-}
-
-
 /**
  * - Lorsqu’on ajoute un produit au panier, si celui-ci n'était pas déjà présent dans le panier, on ajoute un nouvel élément dans l’array.
- * - Lorsqu’on ajoute un produit au panier, si celui-ci était déjà présent dans le panier (même id + même couleur), on
- * incrémente simplement la quantité du produit correspondant dans l’array
+ * - Lorsqu’on ajoute un produit au panier, si celui-ci était déjà présent dans le panier (même id + même couleur), on incrémente simplement la quantité du produit correspondant dans l’array
  * @param {Object} product
  */
  function addProductToLocalStorage(product) {
@@ -100,7 +93,7 @@ addProductCart.onclick = () =>{
         // - Mettre à jour le LS avec la nouvelle valeur
     if (localStorageHas(PRODUCT_KEY_LOCALSTORAGE)) {
         const array = localStorageGet(PRODUCT_KEY_LOCALSTORAGE);
-        const object = array.find(_product => (_product.id === product.id) && (_product.colors === product.colors) && (_product.title === product.title) && (_product.price === product.price));
+        const object = array.find(_product => (_product.id === product.id) && (_product.colors === product.colors) && (_product.title === product.title) && (_product.price === product.price) && (_product.image === product.image) && (_product.imageAlt === product.imageAlt)) ;
 
         if (object) {
             object.quantity = Number(product.quantity) + Number(object.quantity);
@@ -115,21 +108,15 @@ addProductCart.onclick = () =>{
     }
 }
 
-console.log(imageAlt); 
-
 /**
- * 
  * Fonction de personnalisation du bouton "Ajouter au panier"
- * 
  */
-function btnChange(choice) {
+function btnChange() {
     const btn = document.getElementById('addToCart');
+    const defaultValue = 'Sélectionnez une couleur';
+    const selectedValue = colors.value;
 
-    if (choice.value != '') {
-        btn.disabled = false;
-    } 
-    else {
-        btn.disabled = true;
-    }
+    btn.disabled = selectedValue === defaultValue;
 }
-btnChange();
+
+init();
